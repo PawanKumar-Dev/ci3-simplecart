25e709d5783c97e4e0b042882e23a267384e0ecb
Pawan <pk687376@gmail.com>
2022-03-27 15:44:11 +0530

    updated instamojo

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -8,19 +8,19 @@ class Products extends CI_Controller
 		parent::__construct();
 
 		$this->load->library('cart');
-		$this->load->model('productModel');
+		$this->load->model('Productmodel');
 	}
 
 	public function index()
 	{
-		$data['products'] = $this->productModel->getAllProducts();
+		$data['products'] = $this->Productmodel->getAllProducts();
 
 		$this->load->view('products', $data);
 	}
 
 	public function addtocart($proid)
 	{
-		$result = $this->productModel->getSingleProduct($proid);
+		$result = $this->Productmodel->getSingleProduct($proid);
 
 		$data = [
 			'id'      => $result->id,
@@ -46,7 +46,7 @@ class Products extends CI_Controller
 		$rowid = $this->input->get('rowid');
 
 		if (!empty($qty) && !empty($rowid)) {
-			
+
 			$data = [
 				'rowid'      => $rowid,
 				'qty'     => $qty
@@ -60,14 +60,13 @@ class Products extends CI_Controller
 		} else {
 			echo "err";
 		}
-		
 	}
 
 
 
 	public function removecart($rowid)
 	{
-		if($this->cart->remove($rowid)) {
+		if ($this->cart->remove($rowid)) {
 			return redirect('products/cart');
 		}
 	}
@@ -75,75 +74,61 @@ class Products extends CI_Controller
 
 	public function checkout()
 	{
-		if ($this->cart->total_items() > 0) {
 
-			$this->load->library('form_validation');
-			$this->form_validation->set_error_delimiters('<div class="text-danger">', '</div>');
-			
-			if($this->form_validation->run('checkout')) {
+		$this->load->library('form_validation');
+		$this->form_validation->set_error_delimiters('<div class="text-danger">', '</div>');
 
-				$data = [
-							'name' => $this->input->post('name'),
-							'email' => $this->input->post('email'),
-							'phone' => $this->input->post('phone'),
-							'address' => $this->input->post('address'),
-							'created' => date("d-m-Y h:i:s"),
-							'modified' => date("d-m-Y h:i:s"),
-							'status'=> 1
-						];
+		if ($this->form_validation->run('checkout')) {
 
-				if($customer_id = $this->productModel->insertCustomerData($data)) {
-					
-					$orderdata = [
-                            'customer_id' => $customer_id,
-                            'grand_total' => $this->cart->total(),
-                            'created' => date("d-m-Y h:i:s"),
-                            'modified' => date("d-m-Y h:i:s"),
-                            'status'=> 1
-                        ];
+			$name = $this->input->post('name');
+			$email = $this->input->post('email');
+			$phone = $this->input->post('phone');
+			$address = $this->input->post('address');
+			$pricetopay = $this->input->post('pricetopay');
+			$product_name = $this->input->post('product_name');
 
-                    if($order_id = $this->productModel->insertOrdersData($orderdata)) {
+			include 'src/instamojo.php';
 
-                       foreach ($this->cart->contents() as $cart) {
-		                    $product_id = $cart['id'];
-		                    $qty = $cart['qty'];
-		                    $subtotal = $cart['subtotal'];
+			$api = new Instamojo\Instamojo('test_9d5390efa5f237fd7b0b8d294d9', 'test_f0cde5c67c48d9ed959519720d5', 'https://test.instamojo.com/api/1.1/');
 
-		                    $orderitems = [
-	                    		'order_id' => $order_id,
-	                    		'product_id' => $product_id,
-	                    		'quantity' => $qty,
-	                    		'sub_total' => $subtotal
-	                    	];
+			try {
+				$response = $api->paymentRequestCreate(array(
+					"purpose" => $product_name,
+					"amount" => $pricetopay,
+					"buyer_name" => $name,
+					"phone" => $phone,
+					"send_email" => true,
+					"send_sms" => true,
+					"email" => $email,
+					"mobile" => $phone,
+					"shipping_city" => $address,
+					'allow_repeated_payments' => false,
+					"redirect_url" => base_url('products/thankyou'),
+					"webhook" => base_url('products/webhook')
+				));
 
-	                    	$final = $this->productModel->insertOrderItems($orderitems);
-		               }
+				$pay_ulr = $response['longurl'];
 
-                    	
-                    	if($final) {
-                    		$this->cart->destroy();
-                    		return redirect('products/orderdetail');
-                    	}
-                    }
-
-				} else {
-					echo "checkout failed";
-				}
-
-			} else {
-				$this->load->view('checkout');
+				header("Location: $pay_ulr");
+			} catch (Exception $e) {
+				print('Error: ' . $e->getMessage());
 			}
 
+			$this->cart->destroy();
+
 		} else {
-			return redirect('products/cart');
+			$this->load->view('checkout');
 		}
-		
 	}
 
 
-
-	public function orderdetail()
+	public function thankyou()
 	{
-		$this->load->view('orderdetail');
+		$this->load->view('thankyou');
+	}
+
+	public function webhook()
+	{
+		$this->load->view('webhook');
 	}
 }
--- application/models/productModel.php
+++ application/models/productModel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class productModel extends CI_Model
+class Productmodel extends CI_Model
 {
 
         public function getAllProducts()
